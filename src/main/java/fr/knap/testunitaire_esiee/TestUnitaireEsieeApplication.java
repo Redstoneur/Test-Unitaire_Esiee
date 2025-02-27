a8c243d09ab67e26cfa4b76c49b8afcc0dca876f
Hajïn_ <44238262+AHajin@users.noreply.github.com>
2025-02-27 15:00:46 +0100

    Init : Projet
    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/fr/knap/testunitaire_esiee/TestUnitaireEsieeApplication.java
@@ -0,0 +1,13 @@
+package fr.knap.testunitaire_esiee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnitaireEsieeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestUnitaireEsieeApplication.class, args);
+	}
+
+}
