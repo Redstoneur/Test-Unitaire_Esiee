9ed109aa0045d18828e8676acd75392f358018f3
Redstoneur <alfa.aog36@gmail.com>
2025-03-05 22:46:55 +0100

    test: add unit tests to verify application startup and context loading

--- Backend/src/test/java/fr/knap/testunitaire_esiee/TestUnitaireEsieeApplicationTests.java
+++ Backend/src/test/java/fr/knap/testunitaire_esiee/TestUnitaireEsieeApplicationTests.java
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestUnitaireEsieeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void applicationStartsSuccessfully() {
+        TestUnitaireEsieeApplication.main(new String[] {});
+    }
 
+    @Test
+    void applicationContextLoads() {
+        // This test will automatically fail if the application context cannot start
+    }
 }
