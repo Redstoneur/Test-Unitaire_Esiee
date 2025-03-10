d2b44bbf2f8a61dc8f71329a95b48296efd69e90
Redstoneur <alfa.aog36@gmail.com>
2025-03-10 10:12:56 +0100

    refactor: add projectId to Cypress config and update package.json with start script

--- Frontend/cypress.config.ts
+++ Frontend/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'y3hntm',
   e2e: {
     baseUrl: 'http://localhost:5173', // Mets ici l'URL de ton frontend Vue
   },
