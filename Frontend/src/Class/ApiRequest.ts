48e45fdb7fb31c37789fcde3507182cfc8387d81
Redstoneur <alfa.aog36@gmail.com>
2025-03-11 09:58:17 +0100

    feat: refactor ApiRequest class to namespace and export functions

--- Frontend/src/Class/ApiRequest.ts
+++ Frontend/src/Class/ApiRequest.ts
@@ -4,14 +4,14 @@ import ObjetDTO from "../Types/ObjetDTO";
 
 
 /**
- * Class representing API requests.
+ * Class that contains methods to send requests to the API.
  */
-class ApiRequest {
+namespace ApiRequest {
 
     /**
      * The base URL of the API.
      */
-    private static readonly baseUrl = "http://localhost:3000/api";
+    const baseUrl = "http://localhost:3000/api";
 
     /**
      * Sends a login request with the provided username and password.
@@ -20,9 +20,9 @@ class ApiRequest {
      * @param {string} mdp - The password of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async Login(mail: string, mdp: string): Promise<Response | Error> {
+    export async function Login(mail: string, mdp: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/credentials/login`,
+            `${baseUrl}/credentials/login`,
             RequestType.POST,
             {
                 "Content-Type": "application/json"
@@ -41,10 +41,10 @@ class ApiRequest {
      * @param {string} prenom - The first name of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async Register(pseudo: string, mdp: string, mail: string,
+    export async function Register(pseudo: string, mdp: string, mail: string,
                                  nom: string, prenom: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/credentials/register`,
+            `${baseUrl}/credentials/register`,
             RequestType.POST,
             {
                 "Content-Type": "application/json"
@@ -59,9 +59,9 @@ class ApiRequest {
      * @param {string} token - The token to verify.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async VerifyToken(token: string): Promise<Response | Error> {
+    export async function VerifyToken(token: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/credentials/verifyToken`,
+            `${baseUrl}/credentials/verifyToken`,
             RequestType.POST,
             {
                 "Content-Type": "application/json"
@@ -76,8 +76,8 @@ class ApiRequest {
      * @param {string} token - The token to verify.
      * @returns {Promise<boolean>} A promise that resolves to true if the token is valid, false otherwise.
      */
-    public static async BooleanVerifyToken(token: string): Promise<boolean> {
-        const response = await this.VerifyToken(token);
+    export async function BooleanVerifyToken(token: string): Promise<boolean> {
+        const response = await VerifyToken(token);
 
         if (response instanceof Error) {
             return false;
@@ -92,9 +92,9 @@ class ApiRequest {
      * @param {string} token - The authentication token of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async UserInformation(token: string): Promise<Response | Error> {
+    export async function UserInformation(token: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/utilisateurs/trans`,
+            `${baseUrl}/utilisateurs/trans`,
             RequestType.GET,
             {
                 "Content-Type": "application/json",
@@ -108,9 +108,9 @@ class ApiRequest {
      *
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async GetObjets(): Promise<Response | Error> {
+    export async function GetObjets(): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/objets`,
+            `${baseUrl}/objets`,
             RequestType.GET,
             {
                 "Content-Type": "application/json"
@@ -125,9 +125,9 @@ class ApiRequest {
      * @param {string} token - The authentication token of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async DeleteObjet(id: number, token: string): Promise<Response | Error> {
+    export async function DeleteObjet(id: number, token: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/objets/${id}`,
+            `${baseUrl}/objets/${id}`,
             RequestType.DELETE,
             {
                 "Content-Type": "application/json",
@@ -143,9 +143,9 @@ class ApiRequest {
      * @param {string} token - The authentication token of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async AddObjet(objet: ObjetDTO, token: string): Promise<Response | Error> {
+    export async function AddObjet(objet: ObjetDTO, token: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/objets`,
+            `${baseUrl}/objets`,
             RequestType.POST,
             {
                 "Content-Type": "application/json",
@@ -164,9 +164,9 @@ class ApiRequest {
      *
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async GetEchanges(): Promise<Response | Error> {
+    export async function GetEchanges(): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/echanges/all`,
+            `${baseUrl}/echanges/all`,
             RequestType.GET,
             {
                 "Content-Type": "application/json"
@@ -181,9 +181,9 @@ class ApiRequest {
      * @param {string} token - The authentication token of the user.
      * @returns {Promise<Response | Error>} A promise that resolves to the response or an error.
      */
-    public static async GetEchange(id: number, token: string): Promise<Response | Error> {
+    export async function GetEchange(id: number, token: string): Promise<Response | Error> {
         return Request(
-            `${this.baseUrl}/echanges/${id}`,
+            `${baseUrl}/echanges/${id}`,
             RequestType.GET,
             {
                 "Content-Type": "application/json",
