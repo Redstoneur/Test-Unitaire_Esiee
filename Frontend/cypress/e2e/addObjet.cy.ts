95d0c66ba59ab556a2134f2dc51f3a0bee4a73f3
Redstoneur <alfa.aog36@gmail.com>
2025-03-11 12:41:06 +0100

    feat: streamline login process in Cypress tests and comment out unused intercepts

--- Frontend/cypress/e2e/addObjet.cy.ts
+++ Frontend/cypress/e2e/addObjet.cy.ts
@@ -2,13 +2,6 @@ describe('Ajout d\'objet avec authentification', () => {
     beforeEach(() => {
         cy.login();  // Appel de la commande login personnalisée
         cy.visit('http://localhost:5173/add-object');
-
-        cy.get('input[type="email"]').type('admin@mail.fr');
-        cy.get('input[type="password"]').type('admin');
-        cy.get('button[type="submit"]').click();
-        cy.wait(1000);
-
-        cy.visit('/add-object');
     });
 
     it('Doit afficher le formulaire d\'ajout d\'objet pour un utilisateur authentifié', () => {
--- Frontend/cypress/support/commands.ts
+++ Frontend/cypress/support/commands.ts
@@ -8,8 +8,14 @@ declare namespace Cypress {
 
 // Commande personnalisée pour simuler une connexion
 Cypress.Commands.add('login', () => {
-    // Simuler la connexion en stockant un jeton d'authentification dans le localStorage a remplacer par le votre
-    localStorage.setItem('authToken', 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0YW1hMkB5YWhvby5mciMxMjM0NTYiLCJpYXQiOjE3NDE1NDg5NzcsImV4cCI6MTc0MTU1MjU3N30.JLaf970HwvuG3l4QgK57IM1zto_J1eiWHu61S-Z7F_8'); // Remplacer par un jeton valide si nécessaire
+    cy.visit('http://localhost:5173/add-object');
+
+    cy.get('input[type="email"]').type('admin@mail.fr');
+    cy.get('input[type="password"]').type('admin');
+    cy.get('button[type="submit"]').click();
+    cy.wait(1000);
+
+    cy.wait(1000);
 });
 
 
